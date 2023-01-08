1284e1b5f63638ec1e05e94185cd36e5c8581108
James <jamesjglee73@gmail.com>
2023-01-07 21:19:56 -0500

    Tailwind Installation
    Added tailwind and added to DockerFile

---
+++ frontend/src/components/NoteModal.tsx
@@ -0,0 +1,8 @@
+function NoteModal(){
+    return (
+        <div>
+            <h1>Note Modal</h1>
+        </div>
+    )
+}
+export default NoteModal;
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,17 @@
+import React from 'react';
+import '../Css/Dashboard.css'
+import Container from '@mui/material/Container';
+import Notes from '../components/Notes';
 function Dashboard(){
 return (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
+    <>
+    <Container>
+        <h1 className="notes_title">Dormakaba Notes</h1>
+        <Notes></Notes>
+    </Container>
+    
+
+    </>
 )
 }
 export default Dashboard;
