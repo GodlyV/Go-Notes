19ed9567a973969c31f755f9cf083edae8af6117
James <jamesjglee73@gmail.com>
2023-01-08 01:34:59 -0500

    Modal Setup
    Setup modal to receive right information

--- frontend/src/components/NoteModal.tsx
+++ frontend/src/components/NoteModal.tsx
@@ -1,8 +1,57 @@
-function NoteModal(){
-    return (
-        <div>
-            <h1>Note Modal</h1>
-        </div>
-    )
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
+import { Stack } from "@mui/system";
+import { useState } from "react";
+
+interface IModalContentEditNotesProps{
+    open: boolean;
+    note: any;
+    onClose:()=> void;
+}
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const NoteModal = ({ open ,note, onClose }: IModalContentEditNotesProps) => {
+    const [title,setTitle] = useState("");
+    const [text,setText] = useState("");
+    //const [createdAt,setCreatedAt] = useState(note.createdAt);
+    const handleClose = () => {
+        onClose();
+    }
+    if(note){     
+        return (
+            <Modal
+                open={open}
+                onClose={handleClose}
+                key={note.nId}
+        
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Stack spacing={5} sx={style} border='ActiveBorder'>
+                  <Input sx={{ fontSize: 50, textAlign: 'center' }} disableUnderline
+                    id="modal-modal-title" defaultValue={note.title}/>
+
+                  <Input id="modal-modal-description" defaultValue={note.text}sx={{ mt: 2 }} disableUnderline/>
+                </Stack>
+              </Modal>  
+        )
+    }
+    return(
+        <div></div>
+    );
+    
 }
 export default NoteModal;
--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -8,8 +8,32 @@ import Grid from '@mui/material/Grid';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 import CardHeader from '@mui/material/CardHeader';
-import {positions} from '@mui/system';
+import NoteModal from './NoteModal';
+
+export interface NoteProps{
+  nId: number;
+  uId:number;
+  title: string;
+  text: string;
+  //createdAt: Date;
+}
+export interface NoteModalProps{
+  open: boolean;
+  note: NoteProps;
+  handleClose:() => void,
+}
 function Notes() {
+
+  const [selected,setSelected] = useState<NoteProps>();
+  const [open, setOpen] = useState(false);
+  const handleOpen = (note: NoteProps) => {
+    setSelected(note);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setSelected(undefined);
+    setOpen(false);
+  };
   const fetchData: () => any = async () => {
     const URL = `http://localhost:3001/notes`;
     const response = await axios.get(URL);
@@ -27,34 +51,39 @@ function Notes() {
     return <div>Error</div>;
   }
   if (isSuccess) {
-    console.log(data)
+    console.log(data);
     return (
       <>
       <div className="w-full mt-10">
         <Grid container spacing={1}>
           {data.data.map((note: any) => (
-             <Card 
-              variant="outlined"
-              id={note.nId}
-              key={note.nId}
-              sx={{
-                width: 225,
-                height: 225,
-                position: "relative",
-                margin: "20px",
-            }}
-             >
-              <CardHeader key={note.nId} titleTypographyProps={{variant:'h6'}} title={note.title}></CardHeader>
-               <CardContent>
-                <Typography variant="body2">{note.text}</Typography>
-               </CardContent>
-               <CardActions sx={{bottom: 'absolute'}}>
-               <Button size="small">Learn More</Button>
-               </CardActions>
-             </Card>
+            <div onClick={()=>handleOpen(note)}>
+              <Card 
+                variant="outlined"
+                id={note.nId}
+                key={note.nId}
+                sx={{
+                  width: 225,
+                  height: "90%",
+                  display:"flex",
+                  flexDirection: "column",
+                  position: "relative",
+                  margin: "20px",
+              }}
+              >
+                <CardHeader titleTypographyProps={{variant:'h6'}} title={note.title}></CardHeader>
+                <CardContent>
+                  <Typography variant="body2">{note.text}</Typography>
+                </CardContent>
+                <CardActions disableSpacing sx={{mt: "auto"}}>
+                </CardActions>
+              </Card>
+            </div>
+              
           ))}
         </Grid>
       </div>
+      <NoteModal open={open} note={selected} onClose={handleClose}></NoteModal>
       </>
     );
   }
